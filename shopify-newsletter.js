a448087b2c48ce8dac09b5065b1faff16aafd20e
Diana <dianavodopivec16@gmail.com>
2025-01-28 17:19:47 -0300

    feat: newsletter animation controls configuration

---
+++ shopify-newsletter.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const $newsletter__modal = document.querySelector("customNewsletter")
+    const $newsletter__close = document.querySelector("customNewsletter__close")
+  
+    setTimeout(() => {
+        $newsletter__modal.classList.add("visible");
+    }, 500); 
+  
+    $newsletter__close.addEventListener("click", () => {
+      $newsletter__modal.classList.remove("visible");
+    });
+});
+  
